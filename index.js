ff6f47095d7351974cf91c30a1a5ea1a4033a5e5
Mouhamed Seck <sos.sn.logement@gmail.com>
2026-05-04 16:37:47 +0100

    update backend

--- index.js
+++ index.js
@@ -82,7 +82,9 @@ app.use(cors());
 app.use(helmet());
 const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587, // 👈 مهم بدل 465
+  secure: false, // 👈 مهم
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
