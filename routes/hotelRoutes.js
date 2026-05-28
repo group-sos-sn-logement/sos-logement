f42c5e5b9f7eba8d986a61325d072c3abaa3cd6c
Mouhamed Seck <sos.sn.logement@gmail.com>
2026-05-28 11:18:28 +0100

    update backend

--- routes/hotelRoutes.js
+++ routes/hotelRoutes.js
@@ -2,72 +2,39 @@ const express = require("express");
 const router = express.Router();
 
 const auth = require("../middleware/auth");
-
-module.exports = router;
-
+const adminOnly = require("../middleware/adminOnly");
 
 const hotelController =
 require("../controllers/hotelController");
 
-
-
-const adminOnly =
-require("../middleware/adminOnly");
-
-
-
-// ===============================
 // إنشاء فندق
-// ===============================
-
 router.post(
     "/",
     auth,
     hotelController.createHotel
 );
 
-
-
-// ===============================
 // صور الفندق
-// ===============================
-
 router.post(
     "/:id/images",
     auth,
     hotelController.addHotelImages
 );
 
-
-
-// ===============================
 // لوجو الفندق
-// ===============================
-
 router.post(
     "/:id/logo",
     auth,
     hotelController.addHotelLogo
 );
 
-
-
-// ===============================
 // الفنادق المقبولة
-// ===============================
-
 router.get(
     "/",
     hotelController.getApprovedHotels
 );
 
-
-
-
-// ===============================
 // الأدمن
-// ===============================
-
 router.get(
     "/admin/hotels",
     auth,
@@ -75,13 +42,7 @@ router.get(
     hotelController.getAllHotelsAdmin
 );
 
-
-
-
-// ===============================
-// الموافقة على الفندق
-// ===============================
-
+// الموافقة
 router.put(
     "/admin/hotels/:id/approve",
     auth,
