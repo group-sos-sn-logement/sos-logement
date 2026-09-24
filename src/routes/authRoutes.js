383b4aedf79295eea316b480a68f42c9eb875001
Mouhamed Seck <sos.sn.logement@gmail.com>
2026-09-24 08:41:32 +0100

    Update backend

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,8 +4,18 @@ const {
     register
 } = require("../controllers/authController");
 
+const {
+    requestRecovery,
+    verifyRecoveryCode,
+    resetPassword
+} = require("../controllers/recoveryController");
+
 const router = express.Router();
 
 router.post("/register", register);
+
+router.post("/recovery/request", requestRecovery);
+router.post("/recovery/verify", verifyRecoveryCode);
+router.post("/recovery/reset", resetPassword);
 
 module.exports = router;
