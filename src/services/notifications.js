779d6f180b0137660e2e17ddf2fd6361304670b2
Mouhamed Seck <sos.sn.logement@gmail.com>
2026-09-25 17:15:55 +0100

    force ipv4 gmail smtp

--- src/services/notifications.js
+++ src/services/notifications.js
@@ -4,8 +4,6 @@ const nodemailer = require("nodemailer");
 /* =========================================================
    GMAIL
 ========================================================= */
-
-const nodemailer = require("nodemailer");
 const dns = require("dns");
 
 dns.setDefaultResultOrder("ipv4first");
