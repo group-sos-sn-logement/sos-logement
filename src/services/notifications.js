4a0c9fc10d4c3c088a1bdecd8be57a3e8125ea30
Mouhamed Seck <sos.sn.logement@gmail.com>
2026-09-25 16:47:18 +0100

    fix smtp gmail render

--- src/services/notifications.js
+++ src/services/notifications.js
@@ -5,29 +5,18 @@ const nodemailer = require("nodemailer");
    GMAIL
 ========================================================= */
 
-const transporter =
-    nodemailer.createTransport({
-        host: "smtp.gmail.com",
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    family: 4,
 
-        port: 587,
-
-        secure: false,
-
-        requireTLS: true,
-
-        family: 4,
-
-        auth: {
-
-            user:
-                process.env.EMAIL_USER,
-
-            pass:
-                process.env.EMAIL_PASSWORD
-
-        }
-
-    });
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD
+    }
+});
 
 
 /* =========================================================
