68688d73353605c08f8dd7d53c20d4c8dce7131d
Mouhamed Seck <sos.sn.logement@gmail.com>
2026-09-25 17:13:58 +0100

    force ipv4 gmail smtp

--- src/services/notifications.js
+++ src/services/notifications.js
@@ -5,12 +5,16 @@ const nodemailer = require("nodemailer");
    GMAIL
 ========================================================= */
 
+const nodemailer = require("nodemailer");
+const dns = require("dns");
+
+dns.setDefaultResultOrder("ipv4first");
+
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
     requireTLS: true,
-    family: 4,
 
     auth: {
         user: process.env.EMAIL_USER,
